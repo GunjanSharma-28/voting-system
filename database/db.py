120e5bfdc405a4d2fba3a08b9101320e6e950aad
Gunjan <gunjanjnv112@gmail.com>
2026-03-19 15:31:35 +0530

    fix render error

--- database/db.py
+++ database/db.py
@@ -1,10 +1,17 @@
 import mysql.connector
 from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
+
 def get_db_connection():
-    return mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME
-    )
+    try:
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+        return connection
+
+    except Exception as e:
+        print("Database connection failed:", e)
+        return None
